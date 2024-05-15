5d1b0e00f9e4627ec7cfc0c5fa05b03e5d4da053
ISK7 <105171814+ISK7@users.noreply.github.com>
2024-05-15 16:55:38 +0300

    add example

---
+++ StructureExample/src/main/java/org/example/A.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class A implements I{
+    @Override
+    public int i() {
+        int res = 0;
+        return res;
+    }
+    public String a1;
+    protected boolean a2;
+    private double a3;
+}
---
+++ StructureExample/src/main/java/org/example/B.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class B extends A{
+    public int b1;
+    private float b2;
+}
